17fd1da6df756ebb1da7f3a922c3f500c5c6d332
oy_runchi <oy_runchi@qq.com>
2020-09-25 15:31:54 +0800

    优化markdown

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import "./index.scss";
-import './lib/oyui.scss'
-import 'github-markdown-css'
-import { createApp } from "vue";
-import App from "./App.vue";
-import { router } from './router';
+import './index.scss';
+import './lib/oyui.scss';
+import 'github-markdown-css';
+import {createApp} from 'vue';
+import App from './App.vue';
+import {router} from './router';
 
 const app = createApp(App);
 app.use(router);
-app.mount("#app");
+app.mount('#app');
--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,5 @@
 import {createWebHashHistory, createRouter} from 'vue-router';
+import {h} from 'vue';
 import Home from './views/Home.vue';
 import Doc from './views/Doc.vue';
 import SwitchDemo from './components/SwitchDemo.vue';
@@ -6,9 +7,10 @@ import ButtonDemo from './components/ButtonDemo.vue';
 import DialogDemo from './components/DialogDemo.vue';
 import TabsDemo from './components/TabsDemo.vue';
 import DocDemo from './components/DocDemo.vue';
-import Intro from './views/Intro.vue';
-import GetStart from './views/GetStart.vue'
-import Install from './views/Install.vue'
+import Markdown from './components/Markdown.vue';
+
+
+const md = path => h(Markdown, {key: path, path: `../md/${path}.md`});
 const history = createWebHashHistory();
 export const router = createRouter({
   history: history,
@@ -19,9 +21,9 @@ export const router = createRouter({
       component: Doc,
       children: [
         {path: '', component: DocDemo},
-        {path: 'intro', component: Intro},
-        {path: 'get-start', component: GetStart},
-        {path: 'install', component: Install},
+        {path: 'intro', component: md('intro')},
+        {path: 'get-start', component: md('get-start')},
+        {path: 'install', component: md('install')},
         {path: 'switch', component: SwitchDemo},
         {path: 'button', component: ButtonDemo},
         {path: 'dialog', component: DialogDemo},
