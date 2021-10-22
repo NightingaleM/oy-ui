d4c643eefe80954a4843107af2b82174072704be
oy_runchi <oy_runchi@qq.com>
2021-10-22 10:35:10 +0800

    timePickers bug修复和功能完善

--- src/components/demo/timePickersDemo_1.vue
+++ src/components/demo/timePickersDemo_1.vue
@@ -3,13 +3,18 @@
 <p></p>
 </demo>
 <template>
-<!--  <TimePickers-->
-<!--      :allowed-hours="allowedHours"-->
-<!--      :allowed-minutes="allowedMinutes"-->
-<!--  />-->
-  <p>-----------------</p>
-<!--      format="ampm"-->
+  --- {{ value1 }} ---
   <TimePickers
+      v-model:picker="value1"
+      :width="500"
+      :allowed-hours="allowedHours"
+      :allowed-minutes="allowedMinutes"
+  />
+  <p>-----------------</p>
+  --- {{ value2 }} ---
+  <TimePickers
+      v-model:picker="value2"
+      format="ampm"
       use-seconds
       :allowed-hours="allowedHours"
       :allowed-minutes="allowedMinutes"
@@ -17,6 +22,7 @@
 </template>
 <script lang="ts">
 import TimePickers from '../../lib/TimePickers/index.vue';
+import {ref} from 'vue';
 
 export default {
   components: {
@@ -24,8 +30,11 @@ export default {
   },
   setup() {
     const allowedHours = v => v % 5;
-    const allowedMinutes = v => v>10 && 50 > v;
+    const allowedMinutes = v => v > 10 && 50 > v;
+    const value1 = ref(null);
+    const value2 = ref('22:44:00');
     return {
+      value1, value2,
       allowedHours,
       allowedMinutes,
     };
--- src/lib/TimePickers/index.vue
+++ src/lib/TimePickers/index.vue
@@ -1,14 +1,22 @@
 <template>
-  <div :class="['oy-time-pickers',`oy-time-pickers-key-${random}`]">
+  <div :class="['oy-time-pickers',`oy-time-pickers-key-${random}`]" :style="{width:parseInt(`${width}`)+'px'}">
     <div class="oy-time-pickers-header">
-      <span @click="changeTarget('h')">{{ currentHour }}</span>:<span @click="changeTarget('m')">{{
-        currentMinute
-      }}</span><span v-if="useSeconds" @click="changeTarget('s')">:{{ currentSecond }}</span>
+      <span @click="changeTarget('h')">{{ currentHour ?? '--' }}</span>:<span @click="changeTarget('m')">{{
+        currentMinute ?? '--'
+      }}</span><span v-if="useSeconds" @click="changeTarget('s')">:{{ currentSecond ?? '--' }}</span>
     </div>
 
     <div class="oy-time-picker-clock">
-      <div class="oy-time-picker-click-panel" @mousedown="pickerEvent">
+      <div class="oy-time-picker-am-pm" v-if="format==='ampm'">
+        <span @click="amOrPm='am'">AM</span>
+        <span @click="amOrPm='pm'">PM</span>
+      </div>
+      <div class="oy-time-picker-click-panel" @mousedown.capture="pickerEvent">
+        <span class="oy-oy-time-picker-needle"
+              :style="needleStyle"
+        ></span>
         <span :class="['oy-time-picker-item',
+        `oy-item-picker-item-${i.value}`,
         {'oy-time-picker-item-enabled': i.enabled},
         {'oy-time-picker-item-visible': i.visible}
         ]"
@@ -23,10 +31,14 @@
   </div>
 </template>
 <script lang="ts">
-import {computed, onMounted, ref} from 'vue';
+import {computed, nextTick, onMounted, ref, watch} from 'vue';
 
 export default {
   props: {
+    width: {
+      type: Number || String,
+      default: 240
+    },
     useSeconds: {
       type: Boolean,
       default: false
@@ -49,13 +61,23 @@ export default {
       validator: v => {
         return v === '24hr' || v === 'ampm';
       }
+    },
+    picker: {
+      type: String || null,
+      validator: v => {
+        return Object.is(v, null) || !v.match(/[a-zA-Z]./g);
+      }
     }
   },
   setup(props, context) {
+    const {emit} = context;
+    const R = 100;
+    const amOrPm = ref('am');
+    const inputValueList = props.picker ? props.picker.split(':') : [null, null, null];
     const random = parseInt(`${Math.random() * 1000}`);
-    const currentHour = ref('');
-    const currentMinute = ref('');
-    const currentSecond = ref('');
+    const currentHour = ref(inputValueList[0]);
+    const currentMinute = ref(inputValueList[1]);
+    const currentSecond = ref(inputValueList[2] ?? null);
     const timeType = ref(props.format === '24hr' ? 24 : 12); // 12 24
     const currentTarget = ref('h'); // h m s
     const PROPS_MAP = {
@@ -80,10 +102,35 @@ export default {
         };
       });
     });
+    const currentValue = computed(() => {
+      const target = currentTarget.value;
+      let value = 0;
+      switch (target) {
+        case 'h':
+          value = currentHour.value;
+          break;
+        case 'm':
+          value = currentMinute.value;
+          break;
+        case 's':
+          value = currentSecond.value;
+          break;
+      }
+      return value;
+    });
     const panelDom = ref(null);
+    watch([currentHour, currentMinute, currentSecond], () => {
+      if (props.useSeconds && currentHour.value && currentMinute.value && currentSecond.value) {
+        emit('update:picker', `${currentHour.value}:${currentMinute.value}:${currentSecond.value}`);
+      } else if (currentHour.value && currentMinute.value) {
+        emit('update:picker', `${currentHour.value}:${currentMinute.value}`);
+      }
+      ;
+    });
+    let needleAngle = 0;
     onMounted(() => {
       panelDom.value = document.querySelector(`.oy-time-pickers-key-${random} .oy-time-picker-click-panel`);
-      // panelDom.value.addEventListener('mousedown', pickerEvent);
+      moveNeedle(currentValue.value);
     });
     const pick = (item) => {
       if (!item.enabled) return;
@@ -104,13 +151,30 @@ export default {
       }
       if (!next) return;
       const nextTarget = target === 's' ? 's' : (props.useSeconds && target === 'm') ? 's' : 'm';
-      changeTarget(nextTarget);
+      setTimeout(() => {
+
+        changeTarget(nextTarget);
+      }, 50);
     };
     const changeTarget = type => {
       currentTarget.value = type;
+      moveNeedle(currentValue.value);
+    };
+    const moveNeedle = value => {
+      const l = pickers.value.length;
+      if (l === 24) {
+        const cellAngle = 360 / (l / 2);
+        const angle = (value > 11 ? value - 12 : value) * cellAngle;
+        setNeedleStyle(angle, value > 11);
+      } else {
+        const cellAngle = 360 / l;
+        const angle = value * cellAngle;
+        setNeedleStyle(angle);
+      }
     };
     const calcCircleStyle = (item, l) => {
       const i = item.value;
+      const contrastTarget = currentTarget.value === 'h' ? currentHour : currentTarget.value === 'm' ? currentMinute : currentSecond;
       if (l === 24) {
         const angle = 360 / (l / 2);
         const p1 = [0, 0];
@@ -122,14 +186,14 @@ export default {
           p2[0] = p1[0] + 50 * Math.cos(angle * i * Math.PI / 180) + 50;
           p2[1] = p1[1] + 50 * Math.sin(angle * i * Math.PI / 180) + 50;
         }
-        return `bottom: ${p2[0]}%;left: ${p2[1]}%`;
+        return `bottom: ${p2[0]}%;left: ${p2[1]}%; ${contrastTarget.value === i ? 'border: 1px solid #ccc;' : ''}`;
       } else {
         const angle = 360 / l;
         const p1 = [0, 0];
         const p2 = [];
         p2[0] = p1[0] + 50 * Math.cos(angle * i * Math.PI / 180) + 50;
         p2[1] = p1[1] + 50 * Math.sin(angle * i * Math.PI / 180) + 50;
-        return `bottom: ${p2[0]}%;left: ${p2[1]}%`;
+        return `bottom: ${p2[0]}%;left: ${p2[1]}%;  ${contrastTarget.value === i ? 'border: 1px solid #ccc;' : ''}`;
       }
     };
     const pickerMoveEvent = ($event) => {
@@ -139,32 +203,45 @@ export default {
     const pickerUpEvent = ($event) => {
       const panel = panelDom.value;
       pickerEventHandle($event, true);
-      panel.removeEventListener('mousemove', pickerMoveEvent);
-      panel.removeEventListener('mouseup', pickerUpEvent);
+      removeEventList();
+    };
+    const removeEventList = () => {
+      const panel = panelDom.value;
+      panel.removeEventListener('mousemove', pickerMoveEvent, true);
+      panel.removeEventListener('mouseup', pickerUpEvent, true);
     };
     const pickerEvent = ($event) => {
       const panel = panelDom.value;
-      panel.addEventListener('mousemove', pickerMoveEvent);
-      panel.addEventListener('mouseup', pickerUpEvent);
-
+      panel.addEventListener('mousemove', pickerMoveEvent, true);
+      panel.addEventListener('mouseup', pickerUpEvent, true);
     };
+    const needleStyle = ref('transform: rotate(0deg);');
     const pickerEventHandle = ($event, next?) => {
       const panel = panelDom.value;
       const target = $event.target;
       let X = $event.offsetX;
       let Y = $event.offsetY;
-      if (target !== panel && [...target.classList].indexOf('oy-time-picker-item') >= 0) {
+      if (target !== panel && ([...target.classList].indexOf('oy-time-picker-item') >= 0)) {
         X = X + target.offsetLeft;
         Y = Y + target.offsetTop;
+      } else if ([...target.classList].indexOf('oy-oy-time-picker-needle') >= 0) {
+        const x_ = +getComputedStyle(target).height.replace('px', '');
+        X = R + Math.sin(needleAngle * Math.PI / 180) * x_;
+        Y = (R * Math.tan(needleAngle * Math.PI / 180) - Math.sin(needleAngle * Math.PI / 180) * x_) / Math.tan(needleAngle * Math.PI / 180);
       }
-      let angle = (Math.atan(Math.abs(X - 100) / Math.abs(Y - 100)) * 180 / Math.PI);
-      if (X - 100 < 0 && Y - 100 <= 0) {
+      let angle = (Math.atan(Math.abs(X - R) / Math.abs(Y - R)) * 180 / Math.PI);
+      if (X - R === 0) {
+        angle = Y - R > 0 ? 180 : 0;
+      } else if (Y - R === 0) {
+        angle = X - R > 0 ? 90 : 270;
+      } else if (X - R < 0 && Y - R < 0) {
         angle = 360 - angle;
-      } else if (X - 100 <= 0 && Y - 100 >= 0) {
+      } else if (X - R < 0 && Y - R > 0) {
         angle = 180 + angle;
-      } else if (X - 100 >= 0 && Y - 100 > 0) {
+      } else if (X - R > 0 && Y - R > 0) {
         angle = 180 - angle;
-      }
+      } else if (X - R > 0 && Y - R < 0) {
+      } else if (X - R === 0 && Y - R === 0) return;
       const l = pickers.value.length;
       if (l !== 24) {
         const cellAngle = 360 / l;
@@ -173,25 +250,32 @@ export default {
         if (i === l) i = 0;
         if (!pickers.value[i].enabled) return;
         setCurrentValue(i, next);
+        setNeedleStyle(i * cellAngle);
       } else {
-        const distance = (X - 100) / Math.sin(angle * Math.PI / 180);
+        const distance = (X - R) / Math.sin(angle * (Math.PI / 180));
         const cellAngle = 360 / (l / 2);
         let i = (angle + cellAngle / 2) / cellAngle;
         i = distance < 55 ? i + 12 : i;
         i = +parseInt(i.toString());
-
         if (i === l) i = 0;
         if (!pickers.value[i].enabled) return;
         setCurrentValue(i, next);
+        setNeedleStyle(((i > 11 ? i - 12 : i) * cellAngle), distance < 55);
       }
     };
+    const setNeedleStyle = (angle, short?) => {
+      needleAngle = angle;
+      needleStyle.value = `transform: rotate(${angle}deg); height: ${short ? 'calc(25% - 4px)' : 'calc(50% - 4px)'};`;
+      // needleStyle.value = `transform: rotate(${angle}deg) scaleY(${short ? 0.5 : 1});`;
+    };
     return {
+      amOrPm,
       pick, pickerEvent,
       pickers, random,
       calcCircleStyle,
-      // minutes, seconds,
       currentTarget, changeTarget,
-      currentHour, currentMinute, currentSecond
+      currentHour, currentMinute, currentSecond,
+      needleStyle
     };
   }
 };
@@ -204,10 +288,16 @@ export default {
 
   .oy-time-picker-clock {
     position: relative;
-    width: 240px;
+    width: 100%;
     height: 320px;
     border: 1px solid #ccc;
     border-radius: 3px;
+
+    .oy-time-picker-am-pm {
+      display: flex;
+      width: 100%;
+      justify-content: space-between;
+    }
 
     .oy-time-picker-click-panel {
       position: absolute;
@@ -239,6 +329,18 @@ export default {
         font-size: 12px;
         text-align: center;
         line-height: 15px;
+      }
+
+      .oy-oy-time-picker-needle {
+        background-color: #00bcd4;
+        height: calc(50% - 4px);
+        width: 2px;
+        bottom: 50%;
+        left: calc(50% - 1px);
+        transform-origin: center bottom;
+        position: absolute;
+        will-change: transform;
+        z-index: 1;
       }
     }
 
